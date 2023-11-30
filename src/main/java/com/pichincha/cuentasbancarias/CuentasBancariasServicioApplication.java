42c9ac3d39a620bad62467904424fd8ee2bda6cc
Alexis Chavarria <joalexiscb@gmail.com>
2023-11-30 01:05:47 -0500

    Primer commit

---
+++ src/main/java/com/pichincha/cuentasbancarias/CuentasBancariasServicioApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.cuentasbancarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentasBancariasServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentasBancariasServicioApplication.class, args);
+	}
+
+}
